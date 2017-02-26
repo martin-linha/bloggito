a3f99ae57993b2f4525f6107b5a6d0266a55a083
Martin Linha <martin.linha6@gmail.com>
2017-02-26 16:09:28 +0100

    adds certifications to import.sql and adds pdf certificates

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,3 +2,10 @@
 insert into user_detail (id, email, password)  values (1, 'martin.linha6@gmail.com', '$2a$06$r9do2/fj5LBaXUsIJzG/OOrsY4ONxNZZp9.uSASjmEnUIlhLPd.iK')
 
 -- certifications
+
+insert into certification VALUES (1, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-803', '../assets/images/certifications/OCAJSE7.pdf', 'Oracle Certified Associate, Java SE 7 Programmer')
+insert into certification VALUES (2, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-804', '../assets/images/certifications/OCPJSE7.pdf', 'Oracle Certified Professional, Java SE 7 Programmer');
+insert into certification VALUES (3, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-810', '../assets/images/certifications/OCPJSE8.pdf', 'Oracle Certified Professional, Java SE 8 Programmer (upgrade)');
+insert into certification VALUES (4, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-898', '../assets/images/certifications/OCEJEE6JPAD.pdf', 'Oracle Certified Expert, Java EE 6 Persistence API Developer');
+insert into certification VALUES (5, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-899', '../assets/images/certifications/OCEJEE6WCD.pdf', 'Oracle Certified Expert, Java EE 6 Web Component Developer');
+insert into certification VALUES (6, '../assets/images/certifications/avaloq-logo.png', 'Avaloq Evolution AG', null, 'Avaloq Certified Professional');
