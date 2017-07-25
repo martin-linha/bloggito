05f4c71e78600b525c2ab9c58cef0b5f06a5b247
martinlinha <martin.linha6@gmail.com>
2017-07-25 22:51:18 +0200

    adds ejbd certification

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -6,8 +6,9 @@ insert into user_detail (id, email, password, github_id, stack_overflow_id)  val
 insert into certification VALUES (1, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-803', '../assets/images/certifications/OCAJSE7.pdf', 'Oracle Certified Associate, Java SE 7 Programmer');
 insert into certification VALUES (2, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-804', '../assets/images/certifications/OCPJSE7.pdf', 'Oracle Certified Professional, Java SE 7 Programmer');
 insert into certification VALUES (3, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-810', '../assets/images/certifications/OCPJSE8.pdf', 'Oracle Certified Professional, Java SE 8 Programmer (upgrade)');
-insert into certification VALUES (4, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-898', '../assets/images/certifications/OCEJEE6JPAD.pdf', 'Oracle Certified Expert, Java EE 6 Persistence API Developer');
-insert into certification VALUES (5, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-899', '../assets/images/certifications/OCEJEE6WCD.pdf', 'Oracle Certified Expert, Java EE 6 Web Component Developer');
-insert into certification VALUES (6, '../assets/images/certifications/avaloq-logo.png', 'Avaloq Evolution AG', null, 'Avaloq Certified Professional');
+insert into certification VALUES (4, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-895', '../assets/images/certifications/OCEJEE6EJBD.pdf', 'Oracle Certified Expert, Java EE 6 Enterprise JavaBeans Developer');
+insert into certification VALUES (5, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-898', '../assets/images/certifications/OCEJEE6JPAD.pdf', 'Oracle Certified Expert, Java EE 6 Persistence API Developer');
+insert into certification VALUES (6, '../assets/images/certifications/oracle-logo.png', 'Oracle, 1z0-899', '../assets/images/certifications/OCEJEE6WCD.pdf', 'Oracle Certified Expert, Java EE 6 Web Component Developer');
+insert into certification VALUES (7, '../assets/images/certifications/avaloq-logo.png', 'Avaloq Evolution AG', null, 'Avaloq Certified Professional');
 
 -- posts
